7544cab652699092f2403897d19f95afde71e2bf
Mike Reaney <tinytange@hotmail.com>
2024-06-02 15:39:35 +0100

    Navbar added, simple carousels added

---
+++ navbar.js
@@ -0,0 +1,27 @@
+document.write(`
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+  <div class="navbar-collapse collapse w-100 dual-collapse2 order-1 order-md-0">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+      <ul class="navbar-nav mb-2 mb-lg-0">
+        <li class="nav-item me-auto">
+          <a class="nav-link active" aria-current="page" href="animals.html">animals</a>
+        </li>
+        <li class="nav-item">
+        <div class= "mx-auto order-0">
+        <a class="navbar-brand mx-auto" href="index.html">
+          <div>MR</div>
+          <div>Photography</div>
+        </a>
+        </div>
+        </li>
+        <li class="nav-item ms-auto">
+          <a class="nav-link active" aria-current="page" href="sky.html">astro</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+`);
