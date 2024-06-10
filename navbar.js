adf028732bc27246085b46c2cda5eb9177db5eb3
Mike Reaney <tinytange@hotmail.com>
2024-06-10 11:30:33 +0100

    Mobile - navbar completed and adapted catalogue

--- navbar.js
+++ navbar.js
@@ -1,29 +1,32 @@
 document.write(`
-<nav class="navbar navbar-expand-md navbar-dark">
-  <div class="navbar-collapse collapse w-100 dual-collapse2">
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-    <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+<nav class="navbar navbar-expand-lg navbar-dark">
+  <div class="container-fluid navbar-coll ">
+  <a href="index.html">
+    <img src="logo.png" class="brand-icon"  width="500" height="600">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01">
       <ul class="navbar-nav">
-        <li class="nav-item me-auto pe-3 pt-1"">
-          <a class="nav-link active nav-a" aria-current="page" href="animals.html">animals</a>
-        </li>
-        <li class="nav-item me-auto pe-3 pt-1">
-        <a class="nav-link active nav-a" aria-current="page" href="sky.html">astro</a>
+        <li class="nav-item">
+					<a class=" nav-link active nav-a" aria-current="page" href="catalogue.html">animals</a>
         </li>
         <li class="nav-item">
-        <div class= "mx-auto order-0">
-        <a class="navbar-brand mx-auto nav-a" href="index.html">
-          <div>MR</div>
-          <div>Photography</div>
-        </a>
-        </div>
+          <a class="nav-link active nav-a" aria-current="page" href="sky.html">astro</a>
         </li>
-        <li class="nav-item me-auto ps-3 pt-1">
+        <li class="nav-item brand">
+          <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto nav-a" href="index.html">
+              <div>MR</div>
+              <div>Photography</div>
+            </a>
+          </div>
+        </li>
+        <li class="nav-item">
           <a class="nav-link active nav-a" aria-current="page" href="about.html">about</a>
         </li>
-        <li class="nav-item ms-auto ps-3 pt-1">
+        <li class="nav-item">
           <a class="nav-link active nav-a" aria-current="page" href="sky.html">contact</a>
         </li>
       </ul>
